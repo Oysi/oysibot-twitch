37616492e3921770cd47ed60a7811a359ab41eaf
oysi <oysi93@gmail.com>
2022-02-11 05:42:14 +0100

    early

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 
-const tmi = require('tmi.js');
-const conf = require("./conf.json");
+const tmi = require("tmi.js");
+
+const conf = require("../conf.json");
 
 const client = new tmi.Client({
 	options: { debug: true },
@@ -13,12 +14,10 @@ const client = new tmi.Client({
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if(self) return;
-
-	if(message.toLowerCase() === '!hello') {
-		// "@alca, heya!"
-		client.say(channel, `@${tags.username}, heya!`);
+client.on("message", (channel, tags, message, self) => {
+	if (self) return;
+	
+	if (message === "!ping") {
+		client.say(channel, `@${tags.username} pong`);
 	}
 });
