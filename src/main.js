cdf12d78438b92318fcdad9367f339d9d3f54df0
oysi <oysi93@gmail.com>
2022-02-15 05:44:08 +0100

    reorganized main.js

--- src/main.js
+++ src/main.js
@@ -33,52 +33,58 @@ client.on("message", (chan, tags, msg, self) => {
 		info.command.on_message(info);
 	}
 	
-	if (info.chan == "urgie") {
-		if (info.tags.username === "urgie") {
-			const result = info.msg.match(/^(.+) is (\d+)\% boy\.$/)
-			if (result) {
-				const name = result[1];
-				const amnt = result[2];
-				
-				if (!info.conf.least_boy) {
-					info.conf.least_boy = {
-						"name": "none",
-						"amnt": "100",
-					};
-				}
-				
-				if (!info.conf.most_boy) {
-					info.conf.most_boy = {
-						"name": "none",
-						"amnt": "0",
-					};
-				}
-				
-				if (Number(amnt) <= Number(info.conf.least_boy.amnt)) {
-					info.conf.least_boy.name = name;
-					info.conf.least_boy.amnt = amnt;
-				}
-				if (Number(amnt) >= Number(info.conf.most_boy.amnt)) {
-					info.conf.most_boy.name = name;
-					info.conf.most_boy.amnt = amnt;
-				}
-				
-				info.update_conf();
-				
-				info.say(
-					`most boy: ${info.conf.most_boy.name} (${info.conf.most_boy.amnt}%)`
-					+ ", "
-					+ `least boy: ${info.conf.least_boy.name} (${info.conf.least_boy.amnt}%)`
-				);
+	if (info.chan === "oysi") custom_oysi(info);
+	if (info.chan === "urgie") custom_urgie(info);
+});
+
+const custom_oysi = (info) => {
+}
+
+const custom_urgie = (info) => {
+	if (info.tags.username === "urgie") {
+		const result = info.msg.match(/^(.+) is (\d+)\% boy\.$/)
+		if (result) {
+			const name = result[1];
+			const amnt = result[2];
+			
+			if (!info.conf.least_boy) {
+				info.conf.least_boy = {
+					"name": "none",
+					"amnt": "100",
+				};
 			}
-		}
-		if ((info.msg === "!resetboy") && (tags.username == "urgie" || tags.username == "oysi")) {
-			info.conf.least_boy.name = "none";
-			info.conf.least_boy.amnt = "100";
-			info.conf.most_boy.name = "none";
-			info.conf.most_boy.amnt = "0";
+			
+			if (!info.conf.most_boy) {
+				info.conf.most_boy = {
+					"name": "none",
+					"amnt": "0",
+				};
+			}
+			
+			if (Number(amnt) <= Number(info.conf.least_boy.amnt)) {
+				info.conf.least_boy.name = name;
+				info.conf.least_boy.amnt = amnt;
+			}
+			if (Number(amnt) >= Number(info.conf.most_boy.amnt)) {
+				info.conf.most_boy.name = name;
+				info.conf.most_boy.amnt = amnt;
+			}
+			
 			info.update_conf();
-			info.say("boys have been reset");
+			
+			info.say(
+				`most boy: ${info.conf.most_boy.name} (${info.conf.most_boy.amnt}%)`
+				+ ", "
+				+ `least boy: ${info.conf.least_boy.name} (${info.conf.least_boy.amnt}%)`
+			);
 		}
 	}
-});
+	if ((info.msg === "!resetboy") && (tags.username === "urgie" || tags.username === "oysi")) {
+		info.conf.least_boy.name = "none";
+		info.conf.least_boy.amnt = "100";
+		info.conf.most_boy.name = "none";
+		info.conf.most_boy.amnt = "0";
+		info.update_conf();
+		info.say("boys have been reset");
+	}
+}
