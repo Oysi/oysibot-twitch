538f143b361738ee178c8178001b5f64ca8ed05c
oysi <oysi93@gmail.com>
2022-02-17 13:11:28 +0100

    fix

--- src/commands/camgirl.js
+++ src/commands/camgirl.js
@@ -3,7 +3,8 @@ const Command = require("../Command.js");
 const command = new Command();
 
 command.on_message = (info) => {
-	info.say(`@${info.tags.username} https://www.youtube.com/watch?v=cA5m3IwwB1w`);
+	info.say(`@${info.tags.username} https://youtu.be/cA5m3IwwB1w`);
+	// info.say(`@${info.tags.username} https://www.youtube.com/watch?v=cA5m3IwwB1w`);
 }
 
 module.exports = command;
