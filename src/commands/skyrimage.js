9e6e7c60f232222187855b1c5128daba1e818874
oysi-desktop <oysi93@gmail.com>
2022-05-19 12:14:20 +0200

    fixed skyrimage command

--- src/commands/skyrimage.js
+++ src/commands/skyrimage.js
@@ -10,14 +10,14 @@ command.on_message = (info) => {
 	const moment_a = moment("2011-11-11 00:00:00");
 	const moment_b = moment();
 	
-	const info = moment.preciseDiff(moment_b, moment_a, true);
+	const dif = moment.preciseDiff(moment_b, moment_a, true);
 	
 	let text = "";
 	
 	text = "Skyrim is ";
-	text += info.years  + " year"  + (info.years  == 0 ? "s" : "")
-	text += info.months + " month" + (info.months == 0 ? "s" : "")
-	text += info.days   + " day"   + (info.days   == 0 ? "s" : "")
+	text += dif.years  + " year"  + (dif.years  == 0 ? "" : "s") + ", ";
+	text += dif.months + " month" + (dif.months == 0 ? "" : "s") + ", ";
+	text += dif.days   + " day"   + (info.days   == 0 ? "" : "s") + " ";
 	text += " old";
 	
 	info.respond(text);
